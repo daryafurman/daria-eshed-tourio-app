dcdb39a059709e5164402392c2efa045391ad0c2
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-02-13 19:01:09 +0100

    Create Form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,19 +5,31 @@
 // }
 
 import dbConnect from "../../../db/connection.js";
-import Places from "../../../db/schemas/place.js";
+import Place from "../../../db/schemas/place.js";
 
 export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
     try {
-      const places = await Places.find();
+      const places = await Place.find();
       return response.status(200).json(places);
-    } catch (error) {
-      throw new Error(error);
+    } catch (e) {
+      return response.status(404).json({ error: e.message });
     }
-  } else {
-    return response.status(404).json({ message: "Not Found" });
+    // } else {
+    //   return response.status(404).json({ message: "Not Found" });
   }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+      return response.status(200).json({ status: "New Place created!" });
+    } catch (e) {
+      console.error(e);
+      return response.status(404).json({ error: e.message });
+    }
+  }
+
+  return response.status(405).json({ message: "Method not allowed" });
 }
